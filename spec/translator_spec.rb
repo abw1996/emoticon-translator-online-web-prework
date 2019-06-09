61b2116eb9970a50a86aaacbb7d0696f57d137c1
Ashton Williams <ashtonwilliams1996@gmail.com>
2019-06-09 20:05:26 +0000

    Automatically backed up by Learn

--- spec/translator_spec.rb
+++ spec/translator_spec.rb
@@ -90,7 +90,7 @@ describe "#get_japanese_emoticon" do
   end
 
 end
-=begin
+
 describe "#get_english_meaning" do
 
   it "accepts two arguments, the YAML file path and the emoticon" do
@@ -121,5 +121,6 @@ describe "#get_english_meaning" do
     sorry_message = "Sorry, that emoticon was not found"
     expect(get_english_meaning("./lib/emoticons.yml", "$#$%{}*")).to eq(sorry_message)
   end
+  
 end
-=end
+
