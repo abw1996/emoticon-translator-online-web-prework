c0622895bd5026480515c8420f882c0687000179
Ashton Williams <ashtonwilliams1996@gmail.com>
2019-06-09 19:55:18 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,6 @@ def load_library(direct)
   libertee = {"get_meaning"=> {} , "get_emoticon"=> {}}
   leeberee = YAML.load_file(direct)
   leeberee.each do |type, arri|
-    binding.pry
     libertee["get_meaning"][arri[1]] = arri[0]
     libertee["get_emoticon"][arri[0]] = arri[1]
   end
